e2c440844bb0d95ef850b56085614dbddae95210
sxxrxm <s2019w19@e-mirim.hs.kr>
2022-03-15 05:00:03 +0900

    Learn Data binding

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ prices[0] }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ prices[1] }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ prices[2] }}만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      products: ["역삼동 원룸", "천호동 원룸", "마포구 원룸"],
+      prices: [50, 60, 70],
+    };
   },
+  components: {},
 };
 </script>
 

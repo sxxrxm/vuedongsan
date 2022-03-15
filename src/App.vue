1b5105970bde5d388c4e37eb7057c6399033b3cf
sxxrxm <s2019w19@e-mirim.hs.kr>
2022-03-15 19:11:09 +0900

    Make modal and learn v-if

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
+  <div class="black-bg" v-if="modal_stat == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="modal_stat = false">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(a, i) in menus" :key="i">{{ a }}</a>
   </div>
 
   <div>
-    <h4>{{ products[0] }}</h4>
+    <img src="./assets/jelly7.png" />
+    <h4 @click="modal_stat = true">{{ products[0] }}</h4>
     <p>{{ prices[0] }} 만원</p>
     <button @click="신고수[0]++">허위매물신고</button>
     <span> 신고수 : {{ 신고수[0] }} </span>
   </div>
   <div>
+    <img src="./assets/jelly8.png" />
     <h4>{{ products[1] }}</h4>
     <p>{{ prices[1] }} 만원</p>
     <button @click="신고수[1]++">허위매물신고</button>
     <span> 신고수 : {{ 신고수[1] }} </span>
   </div>
   <div>
+    <img src="./assets/logo.png" />
     <h4>{{ products[2] }}</h4>
     <p>{{ prices[2] }} 만원</p>
     <button @click="신고수[2]++">허위매물신고</button>
@@ -32,6 +43,7 @@ export default {
       prices: [50, 60, 70],
       menus: ["Home", "Shop", "About"],
       신고수: [0, 0, 0],
+      modal_stat: false,
     };
   },
   methods: {
@@ -61,5 +73,33 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
